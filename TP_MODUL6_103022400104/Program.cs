f0a5de872a115be6ae96c8eff0152491a2a3d1e6
aditarhm <130527935+aditarhm@users.noreply.github.com>
2026-04-05 22:42:32 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400104/Program.cs
+++ TP_MODUL6_103022400104/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+
+        this.title = title;
+        this.playCount = 0;
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount(int count)
+
+            {
+                playCount += count;
+            }
+       
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("=== Track Details ===");
+        Console.WriteLine("ID        : " + id);
+        Console.WriteLine("Title     : " + title);
+        Console.WriteLine("PlayCount : " + playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("Lagu Favorit");
+
+        track.IncreasePlayCount(5);
+        track.PrintTrackDetails();
+
+        track.IncreasePlayCount(20000000);
+    }
+        
+}
+            
--- TP_MODUL6_103022400104/obj/Debug/net10.0/TP_MODUL6_103022400104.AssemblyInfo.cs
+++ TP_MODUL6_103022400104/obj/Debug/net10.0/TP_MODUL6_103022400104.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400104")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a05bc99756e51765860eed43d18c4e4d9385e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400104")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400104")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
